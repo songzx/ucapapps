445e2eb2022bbae6d54933b339e7e87a06fac366
songzx <songzx@ucap.com.cn>
2014-06-04 20:32:40 +0800

    合并

--- components/src/main/java/com/ucap/components/controllers/rest/WebAccountController.java
+++ components/src/main/java/com/ucap/components/controllers/rest/WebAccountController.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -51,8 +53,10 @@ public class WebAccountController {
 	}
 	
 	@RequestMapping("/regedit")
-	public String regedit(@RequestParam HashMap<String, Object> param,Model model) {
+	public String regedit(@RequestParam MultiValueMap<String,String[]> param) {
 		
 		return UrlBasedViewResolver.FORWARD_URL_PREFIX+ "/restful/web/login";
 	}
+	
+	
 }
