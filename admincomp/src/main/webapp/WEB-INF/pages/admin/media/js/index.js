6032f690c2aa7daa23e68678d0171af53d723aa1
songzx <songzx@ucap.com.cn>
2014-06-06 01:45:59 +0800

    汉化主页日期

--- admincomp/src/main/webapp/WEB-INF/pages/admin/media/js/index.js
+++ admincomp/src/main/webapp/WEB-INF/pages/admin/media/js/index.js
@@ -646,64 +646,9 @@ var Index = function () {
 
         initDashboardDaterange: function () {
 
-            $('#dashboard-report-range').daterangepicker({
-                ranges: {
-                    'Today': ['today', 'today'],
-                    'Yesterday': ['yesterday', 'yesterday'],
-                    'Last 7 Days': [Date.today().add({
-                            days: -6
-                        }), 'today'],
-                    'Last 30 Days': [Date.today().add({
-                            days: -29
-                        }), 'today'],
-                    'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
-                    'Last Month': [Date.today().moveToFirstDayOfMonth().add({
-                            months: -1
-                        }), Date.today().moveToFirstDayOfMonth().add({
-                            days: -1
-                        })]
-                },
-                opens: (App.isRTL() ? 'right' : 'left'),
-                format: 'MM/dd/yyyy',
-                separator: ' to ',
-                startDate: Date.today().add({
-                    days: -29
-                }),
-                endDate: Date.today(),
-                minDate: '01/01/2012',
-                maxDate: '12/31/2014',
-                locale: {
-                    applyLabel: 'Submit',
-                    fromLabel: 'From',
-                    toLabel: 'To',
-                    customRangeLabel: 'Custom Range',
-                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                    firstDay: 1
-                },
-                showWeekNumbers: true,
-                buttonClasses: ['btn-danger']
-            },
-
-            function (start, end) {
-                App.blockUI(jQuery("#dashboard"));
-                setTimeout(function () {
-                    App.unblockUI(jQuery("#dashboard"));
-                    $.gritter.add({
-                        title: 'Dashboard',
-                        text: 'Dashboard date range updated.'
-                    });
-                    App.scrollTo();
-                }, 1000);
-                $('#dashboard-report-range span').html(start.toString('MMMM d, yyyy') + ' - ' + end.toString('MMMM d, yyyy'));
-
-            });
-
+        	var daysOfWeek = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
             $('#dashboard-report-range').show();
-
-            $('#dashboard-report-range span').html(Date.today().add({
-                days: -29
-            }).toString('MMMM d, yyyy') + ' - ' + Date.today().toString('MMMM d, yyyy'));
+            $('#dashboard-report-range span').html(Date.today().toString('yyyy年MM月d日')+'&nbsp;&nbsp;'+daysOfWeek[Date.today().getDay()]);
         },
 
         initIntro: function () {
